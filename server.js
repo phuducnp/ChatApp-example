8dcea1de467046149ed2a884623f8c4e9f7c5402
phuduc <phuduc212@gmail.com>
2019-02-09 15:00:35 +0700

    socket setup

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+
+const server = app.listen(4000, () => {
+    console.log('Server is listening to port 4000');
+})
+
+// Static files
+app.use(express.static('public'));
+
+// Socket setup
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('made socket connection', socket.id);
+})
